50b0a53600617877cd2bdf28fc10a8e85547b00e
Filat1 <sergeyffilat@gmail.com>
2020-01-29 11:50:10 -0500

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
-class Index extends React.Component {
+import fetch from 'isomorphic-fetch'
+import Error from 'next/error'
+import StoryList from '../components/StoryList';
+import Layout from '../components/Layout'
+import Link from 'next/link'
 
+class Index extends React.Component {
+  static async getInitialProps({ req, res, query }) {
+    let page
+    let stories;
+    try {
+      page = Number(query.page) || 1
+      const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
+      stories = await response.json();
+    } catch (error) {
+      console.log('error:', error)
+    }
+    return { page, stories }
+  }
   render() {
+    const { stories, page } = this.props;
+    if (stories === undefined) {
+      return <Error statusCode={503} />
+    }
     return (
-      <div>
-        hacker next
-    </div>
+      <Layout title="Hacker Next" description="This is Hucker news clone(description)" backButton={true}>
+        <StoryList stories={stories} />
+
+        <footer>
+          <Link href={`/?page=${page + 1}`} >
+            <a>Click Next ({page + 1}) </a>
+          </Link>
+        </footer>
+
+        <style jsx>{`
+          footer{
+            padding: 1em;
+          }
+          footer a{
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+          `}</style>
+
+      </Layout>
     )
   }
 }
